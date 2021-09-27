79b8a0a67fec500e7fc8c608e0cd7b9fde814c77
Ben <ben.dew21@gmail.com>
2021-09-27 23:21:06 +0100

    Added files missed off of commit

---
+++ CBaS Core/GitIntegration/UI/GitDockContent.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using CBaSCore.GitIntegration.Business;
+using CBaSCore.GitIntegration.UI.Nodes;
+using CBaSCore.Project.Resources;
+
+namespace CBaSCore.GitIntegration.UI
+{
+    public partial class GitDockContent : UserControl
+    {
+        private BaseGitNode _defaultChangelistNode;
+        private BaseGitNode _unversionedFilesNode;
+        
+        public GitDockContent()
+        {
+            InitializeComponent();
+            GitHandler.GetInstance().SetControl(this);
+            
+            GenerateNodes();
+        }
+
+        private void GenerateNodes()
+        {
+            var root = new BaseGitNode("Changes", true, SimpleGitState.NoChange);
+            _defaultChangelistNode = new BaseGitNode("Default Changelist", true, SimpleGitState.NoChange);
+            _unversionedFilesNode = new BaseGitNode("Unversioned Files", true, SimpleGitState.NoChange);
+            root.Items.Add(_defaultChangelistNode);
+            root.Items.Add(_unversionedFilesNode);
+            TreeViewChanges.Items.Add(root);
+        }
+        
+        public void UpdateChanges(List<BaseGitNode> defaultChangeList, List<BaseGitNode> unversionedFiles)
+        {
+            _defaultChangelistNode.Items.Clear();
+            foreach (var item in defaultChangeList)
+            {
+                _defaultChangelistNode.Items.Add(item);
+            }
+            
+            _unversionedFilesNode.Items.Clear();
+            foreach (var item in unversionedFiles)
+            {
+                _unversionedFilesNode.Items.Add(item);
+            }
+        }
+
+        private void CommitChanges(bool shouldPush)
+        {
+            var changedNames = new List<string>();
+            foreach (var node in _defaultChangelistNode.Items)
+            {
+                if (node is BaseGitNode gitNode && gitNode.Checked != false)
+                {
+                    changedNames.Add(gitNode.NodeName);
+                }
+            }
+            
+            foreach (var node in _unversionedFilesNode.Items)
+            {
+                if (node is BaseGitNode gitNode && gitNode.Checked != false)
+                {
+                    changedNames.Add(gitNode.NodeName);
+                }
+            }
+            
+            GitHandler.GetInstance().CommitChanges(changedNames, TextBoxCommitMessage.Text, shouldPush);
+        }
+
+        private void CommitButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            CommitChanges(false);
+        }
+        
+        private void CommitAndPushButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            CommitChanges(true);
+        }
+    }
+}
